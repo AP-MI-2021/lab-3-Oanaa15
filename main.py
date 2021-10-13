3b19a9b918c4741553340a678a70a3734422ba7e
Oanaa15 <91503903+Oanaa15@users.noreply.github.com>
2021-10-13 08:04:14 +0300

    Update main.py

--- main.py
+++ main.py
@@ -46,7 +46,7 @@ def test_get_longest_all_palindromes():
     assert get_longest_all_palindromes([11, 212, 45, 77]) == [11, 212]
     assert get_longest_all_palindromes([66, 88]) == [66, 88]
 
-print(get_longest_all_palindromes([11, 212, 45, 77]))
+
 def media_lst(lst):
     '''
     determina media numerelor dintr-o lista
@@ -79,11 +79,46 @@ def test_get_longest_average_below():
     assert get_longest_average_below([1, 1, 1, 1, 1, 1], 2) == [1, 1, 1, 1, 1, 1]
     assert get_longest_average_below([6, 8, 9, 10], 20) == [6, 8, 9, 10]
 
+def list_div_k(lst, k):
+    '''
+    determina daca toate nr. dintr-o lista sunt divizibile cu k
+    :param lst: lista de n r. intregi
+    :param k: nr. intrg
+    :return: 1 daca toate nr. dintr-o lista sunt divizibile cu k sau 0 in caz contrar
+    '''
+    for x in lst:
+        if x % k != 0:
+            return 0
+    return 1
+
+
+def get_longest_div_k(lst, k):
+    '''
+    determina cea mai lunga subsecventa de numere care sunt divizibile cu un k dat
+    :param lst: lista de nr. intregi
+    :param k: nr. intreg
+    :return: cea mai lunga subsecventa de numere care sunt divizibile cu un k dat
+    '''
+    subsecventaM = []
+    for i in range(len(lst)):
+        for j in range(i, len(lst)):
+            if list_div_k(lst[i:j + 1], k) == 1 and len(lst[i:j + 1]) > len(subsecventaM):
+                subsecventaM = lst[i:j + 1]
+
+    return subsecventaM
+
+def test_get_longest_div_k():
+    assert get_longest_div_k([12, 4, 80, 17, 6], 2) == [12, 4, 80]
+    assert get_longest_div_k([1, 2, 13, 60, 7], 13) == [13]
+    assert get_longest_div_k([12, 4, 81, 17, 6, 44, 12], 2) == [6, 44, 12]
+
+
 def print_menu():
     print("1. Citire lista")
     print("2. Afisare cea mai lunga subsecventa de nr. palindromuri")
     print("3. Afisare cea mai lunga subsecventa de nr. a caror medie nu depaseste un nr. dat")
-    print("4. Iesire")
+    print("4. Afisare cea mai lunga subsecventa de nr. care sunt divizibile cu k")
+    print("5. Iesire")
 
 def citire_lista():
     l = []
@@ -104,9 +139,12 @@ def main():
         elif optiune == "2":
             print(get_longest_all_palindromes(l))
         elif optiune == "3":
-            average = int(input("Dati nr. "))
+            average = int(input("Dati average:  "))
             print(get_longest_average_below(l, average))
-        elif optiune == "4":
+        elif optiune =="4":
+            k = int(input("Dati k: "))
+            print(get_longest_div_k(l, k))
+        elif optiune == "5":
             break
         else:
             print("Optiune gresita! Reincercati!")
